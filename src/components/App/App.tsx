523e107a49da90b4e442c09989693d22ad0b4cd6
Eduardo Vicente <eduardomcv42@gmail.com>
2025-03-13 16:01:35 +0000

    add App component draft

---
+++ src/components/App/App.tsx
@@ -0,0 +1,34 @@
+import { ProductFilter } from "../ProductFilter";
+
+import "./App.css";
+
+const products = window.datastore.getProducts();
+
+export function App() {
+  return (
+    <>
+      <ProductFilter />
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Product Name</th>
+            <th scope="col">Color</th>
+            <th scope="col">Weight (oz)</th>
+            <th scope="col">Category</th>
+            <th scope="col">Wireless</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <td>{product.property_values[0].value}</td>
+              <td>{product.property_values[1].value}</td>
+              <td>{product.property_values[2].value}</td>
+              <td>{product.property_values[3].value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
