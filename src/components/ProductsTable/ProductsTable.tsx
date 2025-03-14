5167e11f7b69cec797a65e16bbbe2f0d3bff86ca
Eduardo Vicente <eduardomcv42@gmail.com>
2025-03-14 17:32:19 +0000

    split filter into separate file

--- src/components/ProductsTable/ProductsTable.tsx
+++ src/components/ProductsTable/ProductsTable.tsx
@@ -1,5 +1,6 @@
-import { OperatorID, Product, PropertyID } from "../../datastoreTypes";
+import { OperatorID, PropertyID } from "../../datastoreTypes";
 
+import { filterProducts } from "./utils";
 import "./ProductsTable.css";
 
 const PRODUCTS = window.datastore.getProducts();
@@ -13,109 +14,8 @@ const PROPERTY_ID_NAME_MAP = PROPERTIES.reduce(
   {} as Record<PropertyID, string>,
 );
 
-// The separator used when splitting the keywords to search
-const SEPARATOR = ", ";
-
 // In this case, the property ids are numbers, but they could be anything else.
 const PROPERTY_ID_ORDER: PropertyID[] = [0, 1, 2, 3, 4];
-
-interface ProductFilters {
-  propertyID: PropertyID;
-  operatorID: OperatorID;
-  inputValue: string;
-  enumeratedSelections: string[];
-}
-
-function filterProducts(
-  products: Product[],
-  filters: ProductFilters,
-): Product[] {
-  const { propertyID, operatorID, inputValue, enumeratedSelections } = filters;
-
-  return products.filter((product) => {
-    const propertyToFilter = product.property_values.find(
-      (property) => property.property_id === propertyID,
-    );
-
-    switch (operatorID) {
-      case "equals": {
-        if (propertyToFilter?.value === undefined) {
-          return false;
-        }
-
-        if (inputValue.length > 0) {
-          return String(propertyToFilter.value) === inputValue;
-        }
-
-        // My assumption is that, for enumerable properties, the "equals" operator requires all selected
-        // values to match the property's values exactly. Otherwise, it would function the same as the "Is any of" operator.
-        // However, our datastore doesn't seem to support multiple property values for now.
-        // Still, I added the possibility to read comma separated keywords in property values.
-        if (enumeratedSelections.length > 0) {
-          const values = String(propertyToFilter.value).split(SEPARATOR);
-
-          return enumeratedSelections.every((selection) =>
-            values.includes(selection),
-          );
-        }
-
-        return false;
-      }
-      case "greater_than": {
-        const propertyValue = Number(propertyToFilter?.value);
-        const convertedInputValue = Number(inputValue);
-
-        if (Number.isNaN(propertyValue) || Number.isNaN(convertedInputValue)) {
-          return false;
-        }
-
-        return propertyValue > convertedInputValue;
-      }
-      case "less_than": {
-        const propertyValue = Number(propertyToFilter?.value);
-        const convertedInputValue = Number(inputValue);
-
-        if (Number.isNaN(propertyValue) || Number.isNaN(convertedInputValue)) {
-          return false;
-        }
-
-        return propertyValue < convertedInputValue;
-      }
-      case "any": {
-        return propertyToFilter?.value !== undefined;
-      }
-      case "none": {
-        return propertyToFilter?.value === undefined;
-      }
-      case "in": {
-        if (propertyToFilter?.value === undefined) {
-          return false;
-        }
-
-        let values: string[] = [];
-
-        if (inputValue.length > 0) {
-          values = inputValue.split(SEPARATOR);
-        }
-
-        if (enumeratedSelections.length > 0) {
-          values = enumeratedSelections;
-        }
-
-        return values.some((val) => val === String(propertyToFilter.value));
-      }
-      case "contains": {
-        if (propertyToFilter?.value === undefined) {
-          return false;
-        }
-
-        return String(propertyToFilter.value)
-          .toLowerCase()
-          .includes(inputValue.toLowerCase());
-      }
-    }
-  });
-}
 
 export interface ProductsTableProps {
   selectedPropertyID: PropertyID | null;
---
+++ src/components/ProductsTable/utils.ts
@@ -0,0 +1,102 @@
+import { PropertyID, OperatorID, Product } from "../../datastoreTypes";
+
+// The separator used when splitting the keywords to search
+const SEPARATOR = ", ";
+
+export interface ProductFilters {
+  propertyID: PropertyID;
+  operatorID: OperatorID;
+  inputValue: string;
+  enumeratedSelections: string[];
+}
+
+export function filterProducts(
+  products: Product[],
+  filters: ProductFilters,
+): Product[] {
+  const { propertyID, operatorID, inputValue, enumeratedSelections } = filters;
+
+  return products.filter((product) => {
+    const propertyToFilter = product.property_values.find(
+      (property) => property.property_id === propertyID,
+    );
+
+    switch (operatorID) {
+      case "equals": {
+        if (propertyToFilter?.value === undefined) {
+          return false;
+        }
+
+        if (inputValue.length > 0) {
+          return String(propertyToFilter.value) === inputValue;
+        }
+
+        // My assumption is that, for enumerable properties, the "equals" operator requires all selected
+        // values to match the property's values exactly. Otherwise, it would function the same as the "Is any of" operator.
+        // However, our datastore doesn't seem to support multiple property values for now.
+        // Still, I added the possibility to read comma separated keywords in property values.
+        if (enumeratedSelections.length > 0) {
+          const values = String(propertyToFilter.value).split(SEPARATOR);
+
+          return enumeratedSelections.every((selection) =>
+            values.includes(selection),
+          );
+        }
+
+        return false;
+      }
+      case "greater_than": {
+        const propertyValue = Number(propertyToFilter?.value);
+        const convertedInputValue = Number(inputValue);
+
+        if (Number.isNaN(propertyValue) || Number.isNaN(convertedInputValue)) {
+          return false;
+        }
+
+        return propertyValue > convertedInputValue;
+      }
+      case "less_than": {
+        const propertyValue = Number(propertyToFilter?.value);
+        const convertedInputValue = Number(inputValue);
+
+        if (Number.isNaN(propertyValue) || Number.isNaN(convertedInputValue)) {
+          return false;
+        }
+
+        return propertyValue < convertedInputValue;
+      }
+      case "any": {
+        return propertyToFilter?.value !== undefined;
+      }
+      case "none": {
+        return propertyToFilter?.value === undefined;
+      }
+      case "in": {
+        if (propertyToFilter?.value === undefined) {
+          return false;
+        }
+
+        let values: string[] = [];
+
+        if (inputValue.length > 0) {
+          values = inputValue.split(SEPARATOR);
+        }
+
+        if (enumeratedSelections.length > 0) {
+          values = enumeratedSelections;
+        }
+
+        return values.some((val) => val === String(propertyToFilter.value));
+      }
+      case "contains": {
+        if (propertyToFilter?.value === undefined) {
+          return false;
+        }
+
+        return String(propertyToFilter.value)
+          .toLowerCase()
+          .includes(inputValue.toLowerCase());
+      }
+    }
+  });
+}
