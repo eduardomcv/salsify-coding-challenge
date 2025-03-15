923bc656da20aa40d419ea58ae7217cce2502ae5
Eduardo Vicente <eduardomcv42@gmail.com>
2025-03-15 00:12:02 +0000

    add test setup and change config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    setupFiles: ["src/setupTests.ts"],
     browser: {
       enabled: true,
       provider: "playwright",
