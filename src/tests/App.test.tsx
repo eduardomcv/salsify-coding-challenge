5b18d0792513ffb3f07d0ff8ff93a991e64681f6
Eduardo Vicente <eduardomcv42@gmail.com>
2025-03-15 13:05:17 +0000

    add tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -24,8 +24,6 @@ describe("App component", () => {
 
     // table header + 6 products = 7 rows
     expect(page.getByRole("row").elements().length).toBe(7);
-
-    const tableHeader = page.getByRole("rowheader");
   });
 
   test("should show input when selecting string or number property", async () => {
@@ -122,7 +120,7 @@ describe("App component", () => {
     expect(page.getByRole("row").elements().length).toBe(7);
   });
 
-  test("should match exactly when using the 'Equals' operator", async () => {
+  test("should match exactly when using the 'equals' operator for non-enumerable properties", async () => {
     render(<App />);
 
     const propertySelector = page.getByRole("combobox", {
@@ -151,7 +149,51 @@ describe("App component", () => {
       .not.toBeInTheDocument();
   });
 
-  test("should apply the 'greater than' operator correctly", async () => {
+  test("should match exactly when using the 'equals' operator for enumerable properties", async () => {
+    render(<App />);
+
+    const propertySelector = page.getByRole("combobox", {
+      name: "Properties",
+    });
+
+    const operatorSelector = page.getByRole("combobox", {
+      name: "Operators",
+    });
+
+    await propertySelector.selectOptions("category");
+    await operatorSelector.selectOptions("Equals");
+
+    const listbox = page.getByRole("listbox");
+    const toolsOption = listbox.getByRole("option", {
+      name: "tools",
+    });
+    const electronicsOption = listbox.getByRole("option", {
+      name: "electronics",
+    });
+
+    await toolsOption.click();
+
+    expect(page.getByRole("row").elements().length).toBe(3);
+
+    const expectedProducts = ["Key", "Hammer"];
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
+
+    await electronicsOption.click();
+    expect(page.getByRole("row").elements().length).toBe(1);
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .not.toBeInTheDocument();
+    }
+  });
+
+  test("should apply the 'greater_than' operator correctly", async () => {
     render(<App />);
 
     const propertySelector = page.getByRole("combobox", {
@@ -170,20 +212,40 @@ describe("App component", () => {
 
     expect(page.getByRole("row").elements().length).toBe(4);
 
-    await expect
-      .element(page.getByRole("cell", { name: "Headphones" }))
-      .toBeInTheDocument();
+    const expectedProducts = ["Headphones", "Keyboard", "Hammer"];
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
+  });
+
+  test("should apply the 'less_than' operator correctly", async () => {
+    render(<App />);
+
+    const propertySelector = page.getByRole("combobox", {
+      name: "Properties",
+    });
+
+    const operatorSelector = page.getByRole("combobox", {
+      name: "Operators",
+    });
+
+    await propertySelector.selectOptions("weight (oz)");
+    await operatorSelector.selectOptions("Is less than");
+
+    const inputElement = page.getByPlaceholder("Filter value...");
+    await inputElement.fill("3");
+
+    expect(page.getByRole("row").elements().length).toBe(2);
 
     await expect
-      .element(page.getByRole("cell", { name: "Keyboard" }))
-      .toBeInTheDocument();
-
-    await expect
-      .element(page.getByRole("cell", { name: "Hammer" }))
+      .element(page.getByRole("cell", { name: "Key" }))
       .toBeInTheDocument();
   });
 
-  test("should apply the 'greater than' operator correctly", async () => {
+  test("should apply the 'any' operator correctly", async () => {
     render(<App />);
 
     const propertySelector = page.getByRole("combobox", {
@@ -194,24 +256,106 @@ describe("App component", () => {
       name: "Operators",
     });
 
-    await propertySelector.selectOptions("weight (oz)");
-    await operatorSelector.selectOptions("Is greater than");
-
-    const inputElement = page.getByPlaceholder("Filter value...");
-    await inputElement.fill("3");
+    await propertySelector.selectOptions("wireless");
+    await operatorSelector.selectOptions("Has any value");
 
     expect(page.getByRole("row").elements().length).toBe(4);
 
-    await expect
-      .element(page.getByRole("cell", { name: "Headphones" }))
-      .toBeInTheDocument();
+    const expectedProducts = ["Headphones", "Cell Phone", "Keyboard"];
 
-    await expect
-      .element(page.getByRole("cell", { name: "Keyboard" }))
-      .toBeInTheDocument();
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
+  });
 
-    await expect
-      .element(page.getByRole("cell", { name: "Hammer" }))
-      .toBeInTheDocument();
+  test("should apply the 'none' operator correctly", async () => {
+    render(<App />);
+
+    const propertySelector = page.getByRole("combobox", {
+      name: "Properties",
+    });
+
+    const operatorSelector = page.getByRole("combobox", {
+      name: "Operators",
+    });
+
+    await propertySelector.selectOptions("wireless");
+    await operatorSelector.selectOptions("Has no value");
+
+    expect(page.getByRole("row").elements().length).toBe(4);
+
+    const expectedProducts = ["Cup", "Key", "Hammer"];
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
+  });
+
+  test("should apply the 'in' operator correctly", async () => {
+    render(<App />);
+
+    const propertySelector = page.getByRole("combobox", {
+      name: "Properties",
+    });
+
+    const operatorSelector = page.getByRole("combobox", {
+      name: "Operators",
+    });
+
+    await propertySelector.selectOptions("category");
+    await operatorSelector.selectOptions("Is any of");
+
+    const listbox = page.getByRole("listbox");
+    const electronicsOption = listbox.getByRole("option", {
+      name: "electronics",
+    });
+    const kitchenwareOption = listbox.getByRole("option", {
+      name: "kitchenware",
+    });
+
+    await electronicsOption.click();
+    await kitchenwareOption.click();
+
+    expect(page.getByRole("row").elements().length).toBe(5);
+
+    const expectedProducts = ["Headphones", "Cell Phone", "Keyboard", "Cup"];
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
+  });
+
+  test("should apply the 'contains' operator correctly", async () => {
+    render(<App />);
+
+    const propertySelector = page.getByRole("combobox", {
+      name: "Properties",
+    });
+
+    const operatorSelector = page.getByRole("combobox", {
+      name: "Operators",
+    });
+
+    await propertySelector.selectOptions("Product Name");
+    await operatorSelector.selectOptions("Contains");
+
+    const inputElement = page.getByPlaceholder("Filter value...");
+    await inputElement.fill("phone");
+
+    expect(page.getByRole("row").elements().length).toBe(3);
+
+    const expectedProducts = ["Headphones", "Cell Phone"];
+
+    for (const product of expectedProducts) {
+      await expect
+        .element(page.getByRole("cell", { name: product }))
+        .toBeInTheDocument();
+    }
   });
 });
